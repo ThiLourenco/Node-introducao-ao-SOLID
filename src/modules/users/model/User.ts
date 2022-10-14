3bc7941afdb3cb18abcf70d1b846e16be1920ae4
Thigo <thiago.l17@yahoo.com>
2022-10-13 22:59:26 -0300

    Case test: CreateUser

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,21 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id?: string;
+  name: string;
+  admin: boolean;
+  email: string;
+  created_at: Date;
+  update_at: Date;
+
+  constructor(id?: string, name?: string, created_at?: Date) {
+    if (!id) this.id = uuidV4();
+
+    if (!created_at) this.created_at = new Date();
+
+    this.update_at = new Date();
+    this.admin = false;
+  }
 }
 
 export { User };
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,23 +19,47 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.id === id);
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
-    // Complete aqui
+    const filteredUsers = this.users.filter(
+      (user) => user.id === receivedUser.id
+    );
+
+    const updatedUser = new User();
+    Object.assign(updatedUser, {
+      id: receivedUser.id,
+      name: receivedUser.name,
+      email: receivedUser.email,
+      admin: true,
+      created_at: receivedUser.created_at,
+    });
+
+    filteredUsers.push(updatedUser);
+    this.users = filteredUsers;
+    return updatedUser;
   }
 
   list(): User[] {
-    // Complete aqui
+    return this.users;
   }
 }
 
